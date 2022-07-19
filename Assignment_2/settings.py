984e6b5a9ed8695b754f452d1396ebe847b920d7
zeelmehta3107 <75499166+zeelmehta3107@users.noreply.github.com>
2022-07-19 19:52:43 +0530

    Update settings.py

--- Assignment_2/settings.py
+++ Assignment_2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2j%$ko35)*hv7p7_&7=wow)+330w7$q_-(35ojg)4!-$s2+(^k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','https://newsvlog.herokuapp.com/']
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
